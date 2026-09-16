45050adb2966d7211e9ae6be7e5db3f44f822820
Mikael Theovaldo <theovaldomikael@gmail.com>
2026-09-16 16:51:46 -0300

    Reusing instaciation

--- src/main/java/java_study/aplication/Main.java
+++ src/main/java/java_study/aplication/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
         SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
 
